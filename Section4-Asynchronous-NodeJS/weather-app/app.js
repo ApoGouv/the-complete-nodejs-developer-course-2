8fb71d4ae553d61d53fcf95c7345ab6a1129a53b
Apostolos Gouvalas <apo.gouv@gmail.com>
2017-09-14 20:53:46 +0300

    Weather App - cmd options + dynamic requests to google maps API

--- Section4-Asynchronous-NodeJS/weather-app/app.js
+++ Section4-Asynchronous-NodeJS/weather-app/app.js
@@ -5,11 +5,31 @@
  * Date    : 14/9/2017
  */
 const request = require('request');
+const yargs = require('yargs');
 
+
+const argv = yargs
+    .options({
+        address: {
+            demand: true,
+            alias: 'a',
+            describe: 'Address to fetch weather for',
+            string: true
+        }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
+
+// encodeURIComponent() accepts a string and transforms it to be URI compatible
+var encodedAddress = encodeURIComponent(argv.address);
+
+// make a request to google maps and expect a json as answer
 request({
-    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=2%20Al.%20Papanastasiou%20Thessaloniki',
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
     json: true
 }, (error, response, body) => {
-    // the last 4 is for indentation. it can be whatever you prefer
-    console.log(JSON.stringify(body, undefined, 4));
+    console.log(`Address   : ${body.results[0].formatted_address}`);
+    console.log(`Latitude  : ${body.results[0].geometry.location.lat}`);
+    console.log(`Longtitude: ${body.results[0].geometry.location.lng}`);
 });
