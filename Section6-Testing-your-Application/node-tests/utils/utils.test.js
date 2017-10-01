2e306597b0f21ce121eabc4fde75cdc963af8c6c
Apostolos Gouvalas <apo.gouv@gmail.com>
2017-10-01 21:41:27 +0300

    Testing - added assertion library: expect (maybe discard mocha for Jest in future?)

--- Section6-Testing-your-Application/node-tests/utils/utils.test.js
+++ Section6-Testing-your-Application/node-tests/utils/utils.test.js
@@ -4,21 +4,49 @@
  * Author  : Apostolos Gouvalas
  * Date    : 1/10/2017
  */
+const expect = require('expect');
+
 const utils = require('./utils');
 
 it('should add two numbers', () => {
     var res = utils.add(33, 11);
 
-    if (res !== 44){
-        throw new Error(`Expected 44, but got ${res}`);
-    }
+    expect(res).toBe(44).toBeA('number');
 });
 
 
 it('should square a number', () => {
     var res = utils.square(5);
 
-    if (res !== 25){
-        throw new Error(`Expected 25, but got ${res}`);
-    }
+    expect(res).toBe(25).toBeA('number');
+});
+
+// it('should expect some values', () => {
+//     expect(12).toNotBe(11);
+//     expect({name: 'Tolios'}).toEqual({name: 'Tolios'});
+//     expect({name: 'tolios'}).toNotEqual({name: 'Tolios'});
+//     expect([2,3,4]).toExclude(1);
+//     expect({
+//         name: 'Tolios',
+//         age: 30,
+//         location: 'Salonika'
+//     }).toInclude({
+//         age:30
+//     });
+// });
+
+
+// should verify first and last names are set
+// assert it includes firstName and lastName with proper values
+it('should set firstName and lastName', () => {
+    var user = {location: 'Salonika', age: 30};
+    var res = utils.setName(user, 'Apostolis Gouvalas');
+
+    expect(user).toEqual(res);
+    expect(res).toInclude({
+        firstName: 'Apostolis',
+        lastName: 'Gouvalas'
+    });
+    expect(res.firstName).toBeA('string');
+    expect(res.lastName).toBeA('string');
 });
