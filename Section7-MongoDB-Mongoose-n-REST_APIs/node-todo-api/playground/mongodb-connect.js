fe8b7d33232d28abeceb52a0ee0347ad9e1d9fad
Apostolos Gouvalas <apo.gouv@gmail.com>
2017-10-02 13:15:19 +0300

    MongoDB - fetch data

--- Section7-MongoDB-Mongoose-n-REST_APIs/node-todo-api/playground/mongodb-connect.js
+++ Section7-MongoDB-Mongoose-n-REST_APIs/node-todo-api/playground/mongodb-connect.js
@@ -5,30 +5,10 @@
  * Date    : 2/10/2017
  */
 // const MongoClient = require('mongodb').MongoClient;
-//const {MongoClient} = require('mongodb');// this is identical to the above
 const {MongoClient, ObjectID} = require('mongodb');// here we destructuring 2 properties from mongodb
 
-// var user = {name: 'Tolios', age:30};
-// // ES6 Object destructuring ~ pull out properties from an object, creating variables.
-// // Destructuring is a way to make new variables from an object's properties
-// var {name} = user;
-// console.log(name);// Tolios
-
-// var obj = new ObjectID(); // Create Unique ids
-// console.log(obj);
-
-/**
- * Host name of the MongoDB instance/Database Name, e.g: mongodb://localhost:27017/TodoApp
- * connect to the mongodb server: mongodb://localhost:27017/
- * the last part is the DB Name. e.g.: TodoApp
- * in Mongo db we don't have to create a DB before we use it.
- * But, Mongo will not actually create the db, before we add some data in it.
- */
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     if (err){
-        // with return here, we stop the function to continue and
-        // print the success message. Alternatively, we can use an
-        // else statement for the success case.
         return console.log('Unable to connect to MongoDB server');
     }
     console.log('Connected to MongoDB server');
