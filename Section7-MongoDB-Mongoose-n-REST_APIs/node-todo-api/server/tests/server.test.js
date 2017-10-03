7d59b275ac92111d1af03c9165a5d3dff6aa62f8
Apostolos Gouvalas <apo.gouv@gmail.com>
2017-10-03 12:34:29 +0300

    MongoDB - Add tests for GET /todos

--- Section7-MongoDB-Mongoose-n-REST_APIs/node-todo-api/server/tests/server.test.js
+++ Section7-MongoDB-Mongoose-n-REST_APIs/node-todo-api/server/tests/server.test.js
@@ -11,11 +11,19 @@ const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
 // beforeEach(), describe() and it() come from Mocha!
+// insertMany() comme from Mongoose.
+
+const todos =[{
+    text: 'First test todo',
+    }, {
+    text: 'Second test todo'
+    }];
 
 // beforeEach() will automatically execute before EACH test case
 beforeEach((done) => {
-    // delete everything, cause in the test we assume we start with empty db
-   Todo.remove({}).then(() => done());
+   Todo.remove({}).then(() => {
+       return Todo.insertMany(todos)
+   }).then(() => done());
 });
 
 describe('POST /todos', () => {
@@ -37,7 +45,7 @@ describe('POST /todos', () => {
                 }
 
                 // here we make the assumption that the DB is initially empty
-                Todo.find().then((todos) => {
+                Todo.find({text}).then((todos) => {
                     expect(todos.length).toBe(1);
                     expect(todos[0].text).toBe(text);
                     done();
@@ -57,10 +65,23 @@ describe('POST /todos', () => {
 
                 //check the DB
                 Todo.find().then((todos) => {
-                    expect(todos.length).toBe(0);
+                    expect(todos.length).toBe(2);
                     done();
                 }).catch((e) => done(e));
             });
     });
 
+});
+
+
+describe('GET /todos', () => {
+   it('should get all todos', (done) => {
+       request(app)
+           .get('/todos')
+           .expect(200)
+           .expect((res) => {
+                expect(res.body.todos.length).toBe(2);
+           })
+           .end(done);
+   });
 });
