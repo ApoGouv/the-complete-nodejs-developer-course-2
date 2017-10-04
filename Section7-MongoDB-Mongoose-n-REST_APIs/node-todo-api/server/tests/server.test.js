ecd8eb99a00a5b3b0b43104ae04eadc8508e85cf
Apostolos Gouvalas <apo.gouv@gmail.com>
2017-10-04 21:03:35 +0300

    MongoDB - Add tests for DELETE /users/me/token

--- Section7-MongoDB-Mongoose-n-REST_APIs/node-todo-api/server/tests/server.test.js
+++ Section7-MongoDB-Mongoose-n-REST_APIs/node-todo-api/server/tests/server.test.js
@@ -326,3 +326,26 @@ describe('POST /users/login', () => {
       });
   });
 });
+
+describe('DELETE /users/me/token', () => {
+  it('should remove auth token on logout', (done) => {
+    request(app)
+      // DELETE /users/me/token
+      .delete('/users/me/token')
+      // Set x-auth equal to token
+      .set('x-auth', users[0].tokens[0].token)
+      // 200
+      .expect(200)
+      .end((err, res) => {
+        if (err){
+          return done(err);
+        }
+
+        // Find user, verify that tokens array has lenngth of zero
+        User.findById(users[0]._id).then((user) => {
+          expect(user.tokens.length).toBe(0);
+          done();
+        }).catch((e) => done(e));
+      });
+  });
+});
