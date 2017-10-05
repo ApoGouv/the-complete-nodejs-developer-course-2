764253cf1c6873159e8a977c287385ea7e728565
Apostolos Gouvalas <apo.gouv@gmail.com>
2017-10-05 11:34:05 +0300

    MongoDB - Update config to use json file

--- Section7-MongoDB-Mongoose-n-REST_APIs/node-todo-api/server/config/config.js
+++ Section7-MongoDB-Mongoose-n-REST_APIs/node-todo-api/server/config/config.js
@@ -4,16 +4,20 @@
  * Author  : Apostolos Gouvalas
  * Date    : 3/10/2017
  */
+
 // Environment Variables
 var env = process.env.NODE_ENV || 'development';
 
 //console.log(process.env); // Log all the Environment Variables
 //console.log('env *****', env);
+// 'production' env is configured by Heroku
+if (env === 'development' || env === 'test'){
+  var config = require('./config.json');
+  var envConfig = config[env];
 
-if (env === 'development') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-} else if (env === 'test') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+  // Object.keys(obj): takes an object and gets all of the keys and returns them as an array
+  // will output [ 'PORT', 'MONGODB_URI' ]
+  Object.keys(envConfig).forEach((key) => {
+    process.env[key] = envConfig[key];
+  });
 }
